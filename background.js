d4d7c95a9d1769d81a6a86ea88f884dbe0eeb629
Gavin Borg <theborg3of5@gmail.com>
2015-08-02 17:01:56 -0500

    Initial push of code and such.

---
+++ background.js
@@ -0,0 +1,28 @@
+﻿
+
+function closeExtraNewTabs() {
+	chrome.windows.getAll({populate: true}, function(windows){
+		// Snag the current window from the masses.
+		var window;
+		for(var i = 0; i < windows.length; i++) {
+			if(windows[i].focused) {
+				window = windows[i];
+				break;
+			}
+		}
+		
+		// Close any new-page-tabs that are not the current one.
+		chrome.tabs.getSelected(null, function(tab) {
+			for(var i =  window.tabs.length - 1; i > 0; i--) {
+				if(window.tabs[i].id !== tab.id && (firstTab.url.indexOf("chrome://newtab/") != -1)) {
+					chrome.tabs.remove(window.tabs[i].id);
+				}
+			}
+		});
+	});
+}
+
+// Schedule functions for events.
+chrome.tabs.onCreated.addListener(closeExtraNewTabs);
+chrome.tabs.onRemoved.addListener(closeExtraNewTabs);
+
